99d9c582886ea198e85e6f4050416d41eaeeac99
tintoll <belselios@gmail.com>
2020-05-20 08:18:49 +0900

    구독 및 구독취소 API 추가 완료

--- client/src/components/views/VideoDetailPage/Sections/Subscribe.js
+++ client/src/components/views/VideoDetailPage/Sections/Subscribe.js
@@ -35,15 +35,48 @@ function Subscribe(props) {
         }) 
     }, [])
 
+    const onSubscribe = () => {
+        let subscribedVariable = {
+            userTo : props.userTo,
+            userFrom : localStorage.getItem('userId')
+        }
+        // 구독중
+        if(Subscribed) {            
+            Axios.post('/api/subscribe/unSubscribe', subscribedVariable)
+            .then(response => {
+                if(response.data.success) {
+                    setSubscribeNumber(SubscribeNumber - 1)
+                    setSubscribed(!Subscribed)
+                }else {
+                    alert('구독취소 실패')
+                }
+            }) 
+
+        // 구독중이 아니라면     
+        }else {
+            Axios.post('/api/subscribe/subscribe', subscribedVariable)
+            .then(response => {
+                if(response.data.success) {
+                    setSubscribeNumber(SubscribeNumber + 1)
+                    setSubscribed(!Subscribed)
+                }else {
+                    alert('구독 실패')
+                }
+            }) 
+
+
+        }
+    }
+
     return (
         <div>
             <button 
                 style={{
-                    backgroundColor : `${Subscribed ?  '#CC0000' : '#AAAAAA'}`, borderRadius : '4px',
+                    backgroundColor : `${Subscribed ?  '#AAAAAA' : '#CC0000' }`, borderRadius : '4px',
                     color : 'white', padding : '10px 16px',
                     fontWeight : '500', fontSize : '1rem', textTransform : 'uppercase'
                 }}
-                onClick
+                onClick={onSubscribe}
             > 
                 {SubscribeNumber} {Subscribed ? 'Subscribed' : 'Subscribe'}
             </button>
--- server/routes/subscribe.js
+++ server/routes/subscribe.js
@@ -6,7 +6,7 @@ const { Subscriber } = require("../models/Subscriber");
 //             Subscribe
 //=================================
 
-router.post('subscribeNumber', (req, res) => {
+router.post('/subscribeNumber', (req, res) => {
     Subscriber.find({userTo : req.body.userTo})
     .exec((err, subscribe) => {
         if(err) return res.status(400).json({success:false, err});
@@ -14,7 +14,7 @@ router.post('subscribeNumber', (req, res) => {
     })
 })
 
-router.post('subscribed', (req, res) => {
+router.post('/subscribed', (req, res) => {
     Subscriber.find({userTo : req.body.userTo, userFrom : req.body.userFrom})
     .exec((err, subscribe) => {
         if(err) return res.status(400).json({success:false, err});
@@ -25,4 +25,23 @@ router.post('subscribed', (req, res) => {
         return res.status(200).json({success:true, subscribed : result})
     })
 })
+
+router.post('/subscribe', (req, res) => {
+    
+    const subscribe = new Subscriber(req.body)
+    subscribe.save((err, subscribe) => {
+        if(err) return res.status(400).json({success:false, err});        
+        return res.status(200).json({success:true})
+    })
+})
+
+router.post('/unSubscribe', (req, res) => {
+    Subscriber.findOneAndDelete({userTo : req.body.userTo, userFrom : req.body.userFrom})
+    .exec((err, doc) => {
+        if(err) return res.status(400).json({success:false, err});        
+        return res.status(200).json({success:true, doc})
+    })
+})
+
+
 module.exports = router;
