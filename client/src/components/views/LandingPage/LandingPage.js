bd7c7419a9a4b4111b2541d90e6698aa9ee52cdb
tintoll <belselios@gmail.com>
2020-05-19 13:39:47 +0900

    에러 수정

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,7 +1,7 @@
 import React,{useEffect, useState} from 'react'
 import { Card, Typography, Row, Col,  Avatar} from 'antd'
 import Axios from 'axios';
-import { moment } from 'moment';
+import moment from 'moment';
 
 const { Title } = Typography;
 const { Meta}  = Card;
@@ -26,31 +26,29 @@ function LandingPage() {
         var minutes = Math.floor(video.duration / 60);
         var seconds = Math.floor( (video.duration - minutes * 60));
 
-        return (
-            <Col lg={6} md={8} xs={24}>
-                <a href={`/video/post/${video._id}`}>
-                    <div style={{ position: 'relative'}}>
-                        <img style={{ width : '100%'}} src={`http://localhost:5000/${video.thumbnail}`} alt="thumbnail" />
-                        <div className="duration">
-                            <span>{minutes} : {seconds}</span>
-                        </div>
-                    </div>
+        return <Col lg={6} md={8} xs={24}>
+            <div style={{ position: 'relative' }}>
+                <a href={`/video/${video._id}`} >
+                <img style={{ width: '100%' }} alt="thumbnail" src={`http://localhost:5000/${video.thumbnail}`} />
+                <div className=" duration"
+                    style={{ bottom: 0, right:0, position: 'absolute', margin: '4px', 
+                    color: '#fff', backgroundColor: 'rgba(17, 17, 17, 0.8)', opacity: 0.8, 
+                    padding: '2px 4px', borderRadius:'2px', letterSpacing:'0.5px', fontSize:'12px',
+                    fontWeight:'500', lineHeight:'12px' }}>
+                    <span>{minutes} : {seconds}</span>
+                </div>
                 </a>
-                <br />
-                <Meta
-                    avatar={
-                        <Avatar src={video.writer.image} />
-                    }
-                    title={video.title}
-                    description=""
-                />
-
-                <span>{video.writer.name}</span>
-                <br/>
-                <span style={{ marginLeft : '3rem'}}>{video.views} views</span> - <span>{moment(video.createAt).format("MMM Do YY")}</span>
-                
-            </Col>
-        )
+            </div><br />
+            <Meta
+                avatar={
+                    <Avatar src={video.writer.image} />
+                }
+                title={video.title}
+            />
+            <span>{video.writer.name} </span><br />
+            <span style={{ marginLeft: '3rem' }}> {video.views}</span>
+            - <span> {moment(video.createdAt).format("MMM Do YY")} </span>
+        </Col>
     });
 
 
